67ebab130e904f7feaa8c0f60b04c0630babe5d0
Collin Richards <richardscollin@gatech.edu>
2025-01-07 11:44:04 -0700

    move files around

--- data/gnome-terminal/setup.sh
+++ data/gnome-terminal/setup.sh
@@ -1,13 +1,14 @@
 # dconf dump /org/gnome/terminal/legacy/keybindings/ > keys.dconf
 
-
 # dconf reset -f /org/gnome/terminal/legacy/profiles:/
 # dconf list /org/gnome/terminal/legacy/profiles:/
+
+# oneliner to help you identify which profile id corresponds to which name in the gui
+# dconf list /org/gnome/terminal/legacy/profiles:/ | awk '{print "/org/gnome/terminal/legacy/profiles:/"$0"visible-name" }' | xargs -I '{}' -n1 /bin/bash -c "dconf read '{}'; echo '{}'"
 
 # make sure id matches one listed above
 # dconf load /org/gnome/terminal/legacy/profiles:/:61832467-da7b-4a03-802f-a6311372a0e8/ < gnome-terminal.dconf
 # dconf load /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ < gnome-terminal.dconf
-
 
 # alternatively for keybindings
 
